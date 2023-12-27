d4c51d5ac40f831a938f6cc9a7a3d01a56116e5c
Aleksandr Kalinin <59827479+befayer@users.noreply.github.com>
2023-12-27 23:18:16 +0400

    Delete app/build directory

--- app/build/resources/test/seed.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO url
-(name, created_at)
-VALUES
-    ('https://en.hexlet.io', '2023-01-01 23:59:50');
-
-INSERT INTO url_check
-(status_code, title, h1, description, url_id, created_at)
-VALUES
-    (200, 'some title', 'some h1', 'some description', 1, '2023-01-01 23:59:50');
