7d57cf8d6acc25cd4d758a0718bfbcf04e9238c4
Aleksandr Kalinin <a.kalinin@haulmont.com>
2023-12-27 21:04:16 +0400

    Fix tests

--- app/src/test/resources/seed.sql
+++ app/src/test/resources/seed.sql
@@ -1,5 +1,9 @@
-INSERT INTO url (id, name, created_at)
-VALUES (1, 'https://github.com', '2023-07-18 20:55:30');
+INSERT INTO url
+(name, created_at)
+VALUES
+    ('https://en.hexlet.io', '2023-01-01 23:59:50');
 
-INSERT INTO url_check (id, status_code, title, h1, description, url_id, created_at)
-VALUES (1, 200, 'some title', 'some h1', 'some description', 1, '2022-01-01 13:57:40');
+INSERT INTO url_check
+(status_code, title, h1, description, url_id, created_at)
+VALUES
+    (200, 'some title', 'some h1', 'some description', 1, '2023-01-01 23:59:50');
